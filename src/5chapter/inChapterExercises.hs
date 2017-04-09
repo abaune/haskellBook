6981e0b5b7c5ac4ead0069768badd251c3deb056
Austin Baune <abaune@dataminr.com>
2017-04-08 20:51:58 -0600

    chapter 5 apply yourself

--- src/5chapter/inChapterExercises.hs
+++ src/5chapter/inChapterExercises.hs
@@ -30,3 +30,20 @@ f :: Num a => a -> a -> a
 f x y = x + y + 3
 
 f' x y = x + y + 3
+
+-- Exercises: Apply Yourself
+-- 1. (++) :: [a] -> [a] -> [a]
+--    myConcat x = x ++ " yo"
+--    [Char] -> [Char]
+-- 2. (*) :: Num a => a -> a -> a
+--    myMult x = (x / 3) * 5
+--    Fractional a => a -> a
+-- 3. take :: Int -> [a] -> [a]
+--    myTake x = take x "hey you"
+--    Int -> [Char]
+-- 4. (>) :: Ord a => a -> a -> Bool
+--    myCom x = x > (length [1..10])
+--    Int -> Bool
+-- 5. (<) :: Ord a => a -> a -> Bool
+--    myAlph x = x < 'z'
+--    Char -> Bool
